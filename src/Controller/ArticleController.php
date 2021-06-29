112e56a55fa2eb1a3c68d3d4c114c33828372268
David Robert <contact.davidrobert@gmail.com>
2021-06-29 19:03:53 +0200

    live symfony twig

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -11,11 +11,75 @@ class ArticleController extends AbstractController
 {
 
     /**
-     * @Route("/article", name="article")
+     * @Route("/articles", name="articles_list")
      */
-    public function articleShow()
+    public function articleList()
     {
-        return $this->render('articleShow.html.twig');
+        $articles = [
+            [
+                'id' => 1,
+                'title' => 'Article 1',
+                'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aperiam assumenda aut cum distinctio explicabo facilis fugit magnam tempora tempore? Aliquid blanditiis commodi delectus ea hic nihil quam qui ratione'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Article 2',
+                'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aperiam assumenda aut cum distinctio explicabo facilis fugit magnam tempora tempore? Aliquid blanditiis commodi delectus ea hic nihil quam qui ratione'
+            ],
+            [
+                'id' => 3,
+                'title' => 'Article 3',
+                'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aperiam assumenda aut cum distinctio explicabo facilis fugit magnam tempora tempore? Aliquid blanditiis commodi delectus ea hic nihil quam qui ratione'
+            ],
+            [
+                'id' => 4,
+                'title' => 'Article 4',
+                'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aperiam assumenda aut cum distinctio explicabo facilis fugit magnam tempora tempore? Aliquid blanditiis commodi delectus ea hic nihil quam qui ratione'
+            ]
+        ];
+
+        $isSidebarDisplayed = true;
+
+        return $this->render('articleList.html.twig', [
+            'articles' => $articles,
+            'isSidebarDisplayed' => $isSidebarDisplayed
+        ]);
+    }
+
+    /**
+     * @Route("/articles/{id}", name="article_show")
+     */
+    public function articleShow($id)
+    {
+
+        $articles = [
+            [
+                'id' => 1,
+                'title' => 'Article 1',
+                'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aperiam assumenda aut cum distinctio explicabo facilis fugit magnam tempora tempore? Aliquid blanditiis commodi delectus ea hic nihil quam qui ratione'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Article 2',
+                'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aperiam assumenda aut cum distinctio explicabo facilis fugit magnam tempora tempore? Aliquid blanditiis commodi delectus ea hic nihil quam qui ratione'
+            ],
+            [
+                'id' => 3,
+                'title' => 'Article 3',
+                'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aperiam assumenda aut cum distinctio explicabo facilis fugit magnam tempora tempore? Aliquid blanditiis commodi delectus ea hic nihil quam qui ratione'
+            ],
+            [
+                'id' => 4,
+                'title' => 'Article 4',
+                'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aperiam assumenda aut cum distinctio explicabo facilis fugit magnam tempora tempore? Aliquid blanditiis commodi delectus ea hic nihil quam qui ratione'
+            ]
+        ];
+
+        $article = $articles[$id - 1];
+
+        return $this->render('articleShow.html.twig', [
+            'article' => $article
+        ]);
     }
 
 }
